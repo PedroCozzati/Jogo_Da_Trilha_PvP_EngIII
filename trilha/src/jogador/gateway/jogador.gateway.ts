c303e2755440b9b6a67ce7e28d1924e432d8dbb6
Gabriel Corá <gabrielcora20@gmail.com>
2023-03-15 17:31:12 -0300

    chore: alterando o cors

--- trilha/src/jogador/gateway/jogador.gateway.ts
+++ trilha/src/jogador/gateway/jogador.gateway.ts
@@ -8,7 +8,7 @@ import { Logger } from 'nestjs-pino';
 import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
-  cors: '*'
+  cors: false
 })
 export class JogadorGateway implements OnGatewayInit, OnGatewayConnection {
   @WebSocketServer()
