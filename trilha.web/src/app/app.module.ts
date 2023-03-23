82c823963a3067c7348f64abf5932ed669d4ecd7
pedro.camillo <pedro.camillo@bv.com.br>
2023-03-23 17:16:45 -0300

    testando

--- trilha.web/src/app/app.module.ts
+++ trilha.web/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, ButtonComponent]
 })
 export class AppModule { }
---
+++ trilha.web/src/app/button/button.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+
+  constructor() {}
+
+  @Input() title = 'ENTRAR';
+}
