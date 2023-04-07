d3334ae3cafbf99ecf096690f7ae3f8416bb95fc
Gabriel Corá <gabrielcora20@gmail.com>
2023-04-07 16:12:25 -0300

    feat: adicao do saga de peca deletada

--- trilha/src/peca/application/sagas/peca.sagas.ts
+++ trilha/src/peca/application/sagas/peca.sagas.ts
@@ -1,6 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { Logger } from 'nestjs-pino';
+import { ofType, Saga } from '@nestjs/cqrs';
+import { Span } from 'nestjs-otel';
+import { PecaDeletadaEvent } from '../../domain/events/impl/peca-deletada.event';
 
 @Injectable()
 export class PecaSagas {
+    constructor(
+        private readonly _logger: Logger,
+    ) { }
 
+    @Span()
+    @Saga()
+    pecaDeletada(events$: Observable<any>): Observable<void> {
+        return events$
+            .pipe(
+                ofType(PecaDeletadaEvent),
+                map(event => {
+                    this._logger.log("inside saga", { event: JSON.stringify(event) });
+                })
+            )
+    }
 }
