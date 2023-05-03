68d58b97507eb9a87892073d635b0eecbdf7e07f
Gabriel Corá <gabrielcora20@gmail.com>
2023-05-02 22:38:09 -0300

    feat: adicionando itens na tela de tabuleiro

--- trilha/src/transacao/application/dto/transacao.dto.ts
+++ trilha/src/transacao/application/dto/transacao.dto.ts
@@ -4,35 +4,7 @@ export class TransacaoDto {
     @IsString()
     readonly _id: string
 
-    readonly isOld: boolean
+    readonly jogador: string
 
-    readonly isInaccurate: boolean
-
-    readonly altitude: number
-
-    @IsString()
-    readonly positionType: string
-
-    readonly secureLocation: any
-
-    readonly secureLocationTs: number
-
-    readonly latitude: number
-
-    readonly floorLevel: number
-
-    readonly horizontalAccuracy: number
-
-    @IsString()
-    readonly locationType: string
-
-    readonly timeStamp: Date
-
-    readonly locationFinished: boolean
-
-    readonly verticalAccuracy: number
-
-    readonly locationMode: any
-
-    readonly longitude: number
+    readonly valor: number
 }
--- trilha/src/transacao/application/sagas/transacao.sagas.ts
+++ trilha/src/transacao/application/sagas/transacao.sagas.ts
@@ -1,6 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { MetricService, Span } from 'nestjs-otel';
+import { Counter,  } from '@opentelemetry/api';
+import { Logger } from 'nestjs-pino';
+import { Observable, map } from 'rxjs';
+import { Saga, ofType } from '@nestjs/cqrs';
+import { TransacaoRegistradaEvent } from '../../domain/events/impl/transacao-registrada.event';
 
 @Injectable()
 export class TransacaoSagas {
+    private numeroTransacoesCounter: Counter;
+    private valorGastoCounter: Counter;
 
+    constructor(
+        private readonly metricService: MetricService,
+        private readonly _logger: Logger,
+    ) {
+        this.numeroTransacoesCounter = this.metricService.getCounter('trilha_count_transacoes_efetuadas', {
+            description: 'A métrica que fica responsável por armazenar a quantidade de transações efetuadas por usuário',
+        });
+        
+        this.valorGastoCounter = this.metricService.getCounter('trilha_count_valor_transacoes_efetuadas', {
+            description: 'A métrica que fica responsável por armazenar o valor total das transações efetuadas por usuário',
+        });
+    }
+
+    @Span()
+    @Saga()
+    transacaoRegistrada(events$: Observable<any>): Observable<void> {
+        return events$
+            .pipe(
+                ofType(TransacaoRegistradaEvent),
+                map(event => {
+                    this._logger.log("inside saga", { event: JSON.stringify(event) });
+                    
+                    this.numeroTransacoesCounter.add(1, { jogador: event.transacaoDto.jogador, valor: event.transacaoDto.valor });
+
+                    this.valorGastoCounter.add(event.transacaoDto.valor, { jogador: event.transacaoDto.jogador, valor: event.transacaoDto.valor });
+                })
+            )
+    }
 }
