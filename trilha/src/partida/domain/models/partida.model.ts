40c6cf51e0066ff13d85b26796bd44a9d70a537c
Helltraz <tadeu.guilherme2002@gmail.com>
2023-05-14 19:26:48 -0300

    feat: informacao do usuario quem fez a jogada e alteracao na forma de registro dela no banco

--- trilha/src/partida/domain/models/partida.model.ts
+++ trilha/src/partida/domain/models/partida.model.ts
@@ -6,6 +6,7 @@ import { Span } from 'nestjs-otel';
 import { JogadaEfetuadaEvent } from '../events/impl/jogada-efetuada.event';
 import { PartidaDeletadaEvent } from '../events/impl/partida-deletada.event';
 import { PartidaDto } from 'src/partida/application/dto/partida.dto';
+import { PartidaModule } from 'src/partida/partida.module';
 
 @Schema({ collection: 'partida' })
 export class Partida extends BaseModel {
@@ -34,37 +35,66 @@ export class Partida extends BaseModel {
 
   @Span()
   async registraPosicoesIniciais() {
+    // [cor da posicao, x axis, y axis]
+
     this.versaoPartida.push([
       [
-        [-4, 4],
-        [-4, 3],
-        [-4, 2],
-        [-4, 1],
-        [-4, 0],
-        [-4, -1],
-        [-4, -2],
-        [-4, -3],
-        [-4, -4],
+        [1, -4, 4],
+        [1, -4, 3],
+        [1, -4, 2],
+        [1, -4, 1],
+        [1, -4, 0],
+        [1, -4, -1],
+        [1, -4, -2],
+        [1, -4, -3],
+        [1, -4, -4],
       ],
       [
-        [4, 4],
-        [4, 3],
-        [4, 2],
-        [4, 1],
-        [4, 0],
-        [4, -1],
-        [4, -2],
-        [4, -3],
-        [4, -4],
+        [2, 4, 4],
+        [2, 4, 3],
+        [2, 4, 2],
+        [2, 4, 1],
+        [2, 4, 0],
+        [2, 4, -1],
+        [2, 4, -2],
+        [2, 4, -3],
+        [2, 4, -4],
       ]
     ])
   }
 
+  // @Span()
+  // async checaPosicaoLivre(pardidaDto: PartidaDto) {
+
+  // throw new ForaDoPlano();
+  // }
+
   @Span()
-  async registraNovaJogada(partidaDto: PartidaDto) {
+  async registraNovaJogada(registraJogada: PartidaDto) {
     // lógica das jogadas
     // erro = throw Exception
-    this.versaoPartida.push(partidaDto.versaoPartida)
+    const novaVersaoPartida = []
+    const posicaoAntiga = registraJogada.versaoPartida[0];
+    const posicaoNova = registraJogada.versaoPartida[1];
+
+    const ultimaVersaoPartida = this.versaoPartida[this.versaoPartida.length - 1]
+    for (let i = 0; i < ultimaVersaoPartida.length; i++) {
+      const element = ultimaVersaoPartida[i];
+      const subArray = [];
+
+
+      for (let j = 0; j < element.length; j++) {
+        if (posicaoAntiga[1] === element[j][1] && posicaoAntiga[2] === element[j][2]) {
+          subArray.push(posicaoNova)
+        }
+        else {
+          subArray.push(element[j])
+        }
+      }
+      novaVersaoPartida.push(subArray);
+    }
+
+    this.versaoPartida.push(novaVersaoPartida)
   }
 
   @Span()
