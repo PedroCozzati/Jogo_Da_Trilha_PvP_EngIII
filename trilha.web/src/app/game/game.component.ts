0d239550c0357c5a23c6bf3507aa855d9e835543
pedroH97 <p.cozzati27@gmail.com>
2023-05-27 17:57:50 -0300

    Feature- corrigindo logica da movimentacao das pecas + animacao das pecas
    Co-authored-by: Gabriel Corá <gabrielcora20@users.noreply.github.com>

--- trilha.web/src/app/game/game.component.ts
+++ trilha.web/src/app/game/game.component.ts
@@ -3,14 +3,19 @@ import { ModalService } from '../_modal';
 import { Jogador } from '../shared/services/jogador';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { fadeAnimation } from './animations';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css', './game.bg.component.css',]
+  styleUrls: ['./game.component.css', './game.bg.component.css',],
+  animations: [
+    fadeAnimation
+  ],
 })
 
 export class GameComponent {
+  state = "closed";
   jogador: Jogador;
   nivel: any = {}
   nivelPeca: any
@@ -20,7 +25,6 @@ export class GameComponent {
   infoTitle: string;
   playerStone: any = '';
   gameSides: any = []
-  tabuleiro: any = []
 
   matrixString: string;
 
@@ -35,32 +39,89 @@ export class GameComponent {
     router.canceledNavigationResolution = 'computed';
   }
 
+  sleep(time: any) {
+    new Promise((resolve, reject) => {
+      setTimeout(resolve, time)
+    });
+  }
+
+
+  tabuleiro: any = [
+    [], []
+  ]
+
+  tabuleiro2 = [
+    [
+      [-4, 4],
+      [-4, 3],
+      [-4, 2],
+      [-4, 1],
+      [-4, 0],
+      [-4, -1],
+      [-4, -2],
+      [-4, -3],
+      [-4, -4],
+    ],
+    [
+      [4, 4],
+      [4, 3],
+      [4, 2],
+      [4, 1],
+      [4, 0],
+      [4, -1],
+      [4, -2],
+      [4, -3],
+      [4, -4],
+    ]
+  ];
+
+  async getTabuleiro() {
+
+    for (let i = 0; i < this.tabuleiro2[0].length; i++) {
+
+      await new Promise((resolve) => setTimeout(resolve, 200))
+      this.deselectStone()
+
+      this.tabuleiro[0][i] = this.tabuleiro2[0][i]
+      this.tabuleiro[1][i] = this.tabuleiro2[1][i]
+      console.log(this.tabuleiro[0][1])
+    }
+  }
+
+
   ngOnInit() {
 
-    this.tabuleiro = [
-      [
-        [-4, 4],
-        [-4, 3],
-        [-4, 2],
-        [-4, 1],
-        [-4, 0],
-        [-4, -1],
-        [-4, -2],
-        [-4, -3],
-        [-4, -4],
-      ],
-      [
-        [4, 4],
-        [4, 3],
-        [4, 2],
-        [4, 1],
-        [4, 0],
-        [4, -1],
-        [4, -2],
-        [4, -3],
-        [4, -4],
-      ]
-    ];
+    this.getTabuleiro()
+
+    // this.tabuleiro = [
+    //   [
+    //     [-4, 4],
+    //     [-4, 3],
+    //     [-4, 2],
+    //     [-4, 1],
+    //     [-4, 0],
+    //     [-4, -1],
+    //     [-4, -2],
+    //     [-4, -3],
+    //     [-4, -4],
+    //   ],
+    //   [
+    //     [4, 4],
+    //     [4, 3],
+    //     [4, 2],
+    //     [4, 1],
+    //     [4, 0],
+    //     [4, -1],
+    //     [4, -2],
+    //     [4, -3],
+    //     [4, -4],
+    //   ]
+
+    // ];
+
+    if (this.state == "closed") {
+      setTimeout(() => this.state = "wided")
+    }
 
     this.gameSides = ['.stoneBlack', '.stoneWhite']
     var random = Math.floor(Math.random() * this.gameSides.length);
@@ -76,6 +137,13 @@ export class GameComponent {
     };
 
     this.consultaPeca(this.nivel.id)
+
+
+    // setTimeout(() => {
+    //   this.openModal('game-draw')
+
+    // }, 5000);
+
 
   }
 
@@ -96,6 +164,7 @@ export class GameComponent {
   }
 
   getMultiplicadorEixoY(coordenada: any[]) {
+
     if (coordenada[0] == 4 || coordenada[0] == -4) {
       return 7.5
     }
@@ -110,6 +179,30 @@ export class GameComponent {
     }
   }
 
+  verificaPecaRemovida(coordenada: any[], indexJogador: number) {
+
+    // var isMill = true;
+
+    // if (isMill) {
+    //   alert(this.pecaSelecionada.coordenada[0])
+    // return coordenada.forEach((ponto, index) => {
+    //   this.pecaSelecionada?.coordenada.splice(0,1)
+    // })
+    // }
+    // else
+    // if (coordenada == this.pecaSelecionada?.coordenada && indexJogador == this.pecaSelecionada?.indexJogador) {
+    // this.deselectStone();
+    // var hasAnyMill = true
+    // if(hasAnyMill){
+    //   this.infoTitle="Você formou um moinho! - Selecione uma peça do seu adversário para removê-la"
+    //   if(this.pecaSelecionada){
+    //    return true
+
+    //   }
+    // }
+    // }
+  }
+
   verificaSelecaoPeca(coordenada: any[], indexJogador: number) {
     if (coordenada == this.pecaSelecionada?.coordenada && indexJogador == this.pecaSelecionada?.indexJogador) {
       return true
@@ -120,8 +213,9 @@ export class GameComponent {
     this.pecaSelecionada = { indexJogador, coordenada }
   }
 
+
   movePeca(coordenada: any) {
-    if(!this.validaMovimentacao(this.pecaSelecionada?.coordenada, coordenada))
+    if (!this.validaMovimentacao(this.pecaSelecionada?.coordenada, coordenada))
       return
 
     coordenada.forEach((ponto, index) => {
@@ -131,13 +225,34 @@ export class GameComponent {
     this.deselectStone();
   }
 
+  obtemProfundidade(coordenada: any[]) {
+    return Math.abs(Boolean(coordenada.at(0)) ? coordenada.at(0) : coordenada.at(1))
+  }
+
+  obtemComprimentoMovimento(origem: any[], destino: any[]) {
+    const indexCoordenada = origem.at(0) != destino.at(0) ? 0 : 1
+    const diferenca = origem.at(indexCoordenada) + (-destino.at(indexCoordenada))
+    return Math.abs(diferenca)
+  }
+
+  isMovimentacaoDiagonal(origem: any[], destino: any[]) {
+    return (origem.at(0) != destino.at(0)) && (origem.at(1) != destino.at(1))
+  }
+
+  isDistanciaCorretaMovimentacao(origem: any[], destino: any[]) {
+    const profundidadeOrigem = this.obtemProfundidade(origem)
+    const profundidadeDestino = this.obtemProfundidade(destino)
+
+    return this.obtemComprimentoMovimento(origem, destino) === (profundidadeDestino !== profundidadeOrigem ? 1 : profundidadeDestino)
+  }
+
   validaMovimentacao(origem: any[], destino: any[]) {
-    debugger
     if (destino && Math.abs(origem?.at(0)) < 4) {
-      if(Math.abs(Math.abs(destino.at(0)) - Math.abs(origem.at(0))) > 1 || Math.abs(Math.abs(destino.at(1)) - Math.abs(origem.at(1))) > 1)
+      if (this.isMovimentacaoDiagonal(origem, destino))
         return false
 
-      return Boolean(Math.abs(Math.abs(origem.at(0)) - Math.abs(destino.at(0)))) !== Boolean(Math.abs(Math.abs(origem.at(1)) - Math.abs(destino.at(1))))
+      if (!this.isDistanciaCorretaMovimentacao(origem, destino))
+        return false
     }
 
     return true
@@ -155,6 +270,14 @@ export class GameComponent {
     this.modalService.close(id);
   }
 
+  closeModalGame(id: string) {
+    var item = {
+      url: `login-authenticated/${JSON.stringify(this.jogador)}`
+    };
+
+    this.ngZone.run(() => this.router.navigateByUrl(item.url));
+  }
+
   corLadoA = 'red'
   corLadoB = 'blue'
 
@@ -169,3 +292,4 @@ export class GameComponent {
 
 
 }
+
